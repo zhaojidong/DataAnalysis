53b12bb7bee002e425d336f5ff18805cf1debe79
unknown <1024083926@qq.com>
2022-04-22 14:11:00 +0800

    new updata

--- HandleLogFIle.py
+++ HandleLogFIle.py
@@ -1,46 +1,86 @@
 import linecache,os,numpy as np,re
 from collections import defaultdict,Counter
-import os
+import os,sys
 import pandas as pd
 
 test_name_dict = {}
 signal_list = []
-log_file_path = r'D:\海图微\HT50A\HT50A\HT50A\CW\10125AE'
+log_file_path = r'D:\Python\Project\DataAnalysis\1009LAE0039'
 pattern1 = re.compile(r'(TCNT#)\s*[0-9](\s*)(SITE#)(\s*)', re.I)
 pattern2 = re.compile(r'-------------------', re.I)
 
 
 def LogFile():
-    write_flag = False
+    pd.set_option('display.width', None)
+
     # Follow Code: get all files name and file count
     file_name_list = os.listdir(log_file_path)
-    file_counter = len(file_name_list)
+    file_total = len(file_name_list)
     # Follow Code: open file and handle
-    for i in range(file_counter):
+    for i in range(file_total):
         file_path = log_file_path + '\\' + file_name_list[i]
         fp = open(file_path,'rb')
-        # print(len(fp.readlines()))
-        # Follow Code: get line count
+        # Follow Code: get line total sum
         count = -1
-        for count, line in enumerate(open(file_path, 'rU')):
+        for count, line in enumerate(open(file_path, 'r')):
             pass
         count += 1
         # Follow Code: read line information
         for line_data in range(count):
             text = linecache.getline(file_path, line_data)
+            # Follow Code: Find the title's line
             if re.search(pattern1, text):
-                write_flag = True
-                line_target = line_data + 1
-                for line_target in range(line_target,count):
+                line_target = line_data + 2
+                title = linecache.getline(file_path, line_data + 1)
+                # Follow Code: Title, convert to list
+                title_list = title.split()
+                title_len = len(title_list)
+                execute_once = True
+                for line_target in range(line_target, count):
                     text = linecache.getline(file_path, line_target)
-                    text.split()
-                    print(text)
-                    pf1 = pd.DataFrame(columns=text.split())
-                    print(pf1)
-                    pass
-            # if write_flag == True:
-            #     text = linecache.getline(file_path, line_data)
-            #
-            #     print(text)
-
+                    # split according to signal space
+                    line = re.split(r"[ ]+", text)
+                    # delete '\n':strip() used for \n and space defaulted
+                    line = [x.strip() for x in line]
+                    # merger the unit with before data
+                    for index,value in enumerate(line):
+                        if value == 'nV' or value == 'uV' or value == 'mV' or value == 'V' \
+                           or value == 'nA' or value == 'uA' or value == 'mA' or value == 'A':
+                            line[index-1] = line[index-1] + line[index]
+                            del line[index]
+                    # list max length, append 'None' to
+                    for add_none_count in range(title_len-len(line)):
+                        line.append('None')
+                    # Follow Code: Add the end label to the pandas dataframe
+                    line_end = ['-']
+                    line_end = line_end * title_len
+                    line_end[0] = '-End of data-'
+                    if str(line[0]).isdigit():
+                        if execute_once:
+                            execute_once = False
+                            NewList = [[x] for x in line]
+                            pd_dict = dict(zip(title_list,NewList))
+                            # print(pd.DataFrame(pd_dict, index=[0]))
+                        else:
+                            line_count = 0
+                            for key in title_list:
+                                pd_dict[key] = pd_dict.get(key,[]) + [line[line_count]]
+                                line_count = line_count + 1
+                                if line_count > len(line)-1:
+                                    break
+                break
+        final_pd = pd.DataFrame(pd_dict)
+        final_pd.loc[len(final_pd)] = line_end
+        return final_pd
         fp.close()
+        break
+
+
+def tryyyyy():
+    dicttry = {'qaz':['q','a','z'],'wsx':['w','s','x']}
+    new_pd_dict = pd.DataFrame(dicttry)
+    print(new_pd_dict)
+
+
+if __name__ == '__main__':
+    LogFile()
--- main.py
+++ main.py
@@ -1,64 +1,135 @@
 import sys
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QBrush, QColor
 from PyQt5.QtCore import Qt
 from DataAnalysis import Ui_MainWindow
+import HandleLogFIle
+import pandas as pd
+from PyQt5.QtWidgets import QTreeWidgetItem, QTreeWidget, QWidget, QVBoxLayout, QPushButton, QApplication
 
 
-
-class HT_DataAnalysis_UI(QMainWindow,Ui_MainWindow):
+class HT_DataAnalysis_UI(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):  # parent=None,so the HaiTu_UI is the topmost window
-        super(HT_DataAnalysis_UI, self).__init__(parent)  # the super().__init__() excutes the constructor fo father, then we can use the property of father
+        super(HT_DataAnalysis_UI, self).__init__(
+            parent)  # the super().__init__() excutes the constructor fo father, then we can use the property of father
         self.init()
 
     def init(self):
         self.setupUi(myWindow)
-        myWindow.setWindowTitle('海图数据分析')
-        # 设置列数
+        myWindow.setWindowTitle('海图微数据分析工具')
+        self.create_tree()
+        self.button_handler()
+
+    def button_handler(self):
+        self.btn_yeild.clicked.connect(lambda: self.get_checked(self.root))  # get_checked(self.root)
+
+    def create_tree(self):
+        pd_get = HandleLogFIle.LogFile()
+        pd_rows = len(pd_get)
+        # set tree's column
         self.tree.setColumnCount(2)
-        # 设置树形控件头部的标题
+        # set the title
         self.tree.setHeaderLabels(['Key', 'Value'])
-
-        # 设置根节点
-        root = QTreeWidgetItem(self.tree)
-        root.setText(0, 'Root')
-        root.setIcon(0,QIcon('./123.jpg'))
-
-        # todo 优化2 设置根节点的背景颜色
-        brush_red = QBrush(Qt.red)
-        root.setBackground(0, brush_red)
-        brush_blue = QBrush(Qt.blue)
-        root.setBackground(1, brush_blue)
-
-        # 设置树形控件的列的宽度
-        self.tree.setColumnWidth(0, 150)
-
-        # 设置子节点1
-        child1 = QTreeWidgetItem()
-        child1.setText(0, 'child1')
-        child1.setText(1, 'ios')
-
-        # todo 优化1 设置节点的状态
-        child1.setCheckState(0, Qt.Checked)
-
-        root.addChild(child1)
-
-        # 设置子节点2
-        child2 = QTreeWidgetItem(root)
-        child2.setText(0, 'child2')
-        child2.setText(1, '')
-
-        # 设置子节点3
-        child3 = QTreeWidgetItem(child2)
-        child3.setText(0, 'child3')
-        child3.setText(1, 'android')
+        self.tree.setColumnWidth(0, 300)
+        self.root = QTreeWidgetItem(self.tree)
+        self.root.setText(0, 'Test Items')
+        self.root.setCheckState(0, Qt.Unchecked)
+        loop_count = 0
+        while loop_count < (pd_rows - 1):
+            TestName = pd_get.iloc[loop_count].at['TestName']
+            if TestName == '-End of data-':
+                break
+            child1 = QTreeWidgetItem()
+            child1.setText(0, TestName)
+            self.root.addChild(child1)
+            child1.setCheckState(0, Qt.Unchecked)
+            while (TestName == pd_get.iloc[loop_count].at['TestName']):
+                SignalName = pd_get.iloc[loop_count].at['Signal']
+                loop_count = loop_count + 1
+                child2 = QTreeWidgetItem()
+                child2.setText(0, SignalName)
+                child1.addChild(child2)
+                child2.setCheckState(0, Qt.Unchecked)
 
         # 加载根节点的所有属性与子控件
-        self.tree.addTopLevelItem(root)
+        self.tree.addTopLevelItem(self.root)
 
         # 节点全部展开
         self.tree.expandAll()
         # self.setCentralWidget(self.tree)
+        self.tree.itemChanged.connect(self.handlechanged)
+        # self.tree.itemChanged.connect(self.get_changable)
+
+    def handlechanged(self, item, column):
+        f_list = list()
+        c_list = list()
+        # 获取选中节点的子节点个数
+        count = item.childCount()
+        # 如果被选中
+        if item.checkState(column) == Qt.Checked:
+            # print("checked", item, item.text(column),column)
+            f_list.append(item.text(column))
+            # print('f_list',f_list)
+            # 连同下面子子节点全部设置为选中状态
+            for baby in range(count):
+                # print('baby',item.child(baby).text(column))
+                c_list.append(item.child(baby).text(column))
+                # print('c_list',c_list)
+                if item.child(baby).checkState(0) != Qt.Checked:
+                    item.child(baby).setCheckState(0, Qt.Checked)
+        # 如果取消选中
+        if item.checkState(column) == Qt.Unchecked:
+            # print("unchecked", item, item.text(column))
+            # 连同下面子子节点全部设置为取消选中状态
+            for baby in range(count):
+                if item.child(baby).checkState != Qt.Unchecked:
+                    item.child(baby).setCheckState(0, Qt.Unchecked)
+
+    def start_convert(self, item):
+        print('start_convert')
+        f_list = list()
+        c_list = list()
+        column = 0
+        item = QtWidgets.QTreeWidgetItemIterator(self.root)
+        print(item)
+        # 获取选中节点的子节点个数
+        count = item.childCount()
+        # 如果被选中
+        if item.checkState(column) == Qt.Checked:
+            # print("checked", item, item.text(column),column)
+            f_list.append(item.text(column))
+            # print('f_list',f_list)
+            # 连同下面子子节点全部设置为选中状态
+            for baby in range(count):
+                if item.child(baby).checkState(0) == Qt.Checked:
+                    c_list.append(item.child(baby).text(column))
+        # 如果取消选中
+        if item.checkState(column) == Qt.Unchecked:
+            # print("unchecked", item, item.text(column))
+            # 连同下面子子节点全部设置为取消选中状态
+            for baby in range(count):
+                if item.child(baby).checkState == Qt.Unchecked:
+                    c_list(item.child(baby).text(column))
+        print(c_list)
+        print(f_list)
+
+    def get_checked(self, node: QTreeWidgetItem) -> list:
+        """ 得到当前节点选中的所有分支， 返回一个 list """
+        temp_list = []
+        # 此处看下方注释 1
+
+        for item in node.takeChildren():
+            print('11111111111')
+            # 判断是否选中
+            if item.checkState(0) == Qt.Checked:
+                temp_list.append(item.text(0))
+                # 判断是否还有子分支
+                if item.childCount():
+                    temp_list.extend(self.get_checked(item))
+            node.addChild(item)
+        print('get_checked', temp_list)
+        return temp_list
 
 
 if __name__ == '__main__':
@@ -66,6 +137,4 @@ if __name__ == '__main__':
     myWindow = QMainWindow()
     HTUI = HT_DataAnalysis_UI()
     myWindow.show()
-    # HTUI.show()
     sys.exit(app.exec_())
-
